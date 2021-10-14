de7db9dccd3d8e4d6571456f9d8b4ca5efd413e5
Beliavsky <38887928+Beliavsky@users.noreply.github.com>
2021-10-14 11:25:09 -0400

    Update rterm.bat

--- rterm.bat
+++ rterm.bat
@@ -1,10 +1,2 @@
 @echo off
 C:\programs\R\R-4.1.0\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-4.0.3\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-4.0.1\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.6.1\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.5.2\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.5.0\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.4.0\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.2.5\bin\x64\rterm.exe --vanilla --slave
-:: C:\programs\R\R-3.0.2\bin\x64\rterm.exe --vanilla --slave
